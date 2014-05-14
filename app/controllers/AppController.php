586e439a376bda4c0816524c4e54feded9426535
Jung Oh <Jung3o@yahoo.com>
2014-05-14 16:15:38 -0500

    redirection error

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -69,7 +69,7 @@ class AppController extends BaseController {
 
     if(!is_numeric($steamCommunityId) || !preg_match('/7656119/', $steamCommunityId))
     {
-      return Redirect::back()->withInput()->with('error', 'Invalid ID');
+      return Redirect::route('home')->withInput()->with('error', 'Invalid ID');
     }
 
     $userInfo = $this->getVBanUser($steamCommunityId);
