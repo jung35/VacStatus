2ca5c3e87e22935edca6cf06f83c0956e58819e9
Jung Oh <Jung3o@yahoo.com>
2014-08-01 10:13:10 -0500

    foundation is now set up in vacstatus using bower

--- config.rb
+++ config.rb
@@ -3,19 +3,20 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "public/css"
 sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+images_dir = "public/img"
+javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
