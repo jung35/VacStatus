0bfb01493f817690aba0a3126d71eeeb39e6dee2
Jung Oh <Jung3o@yahoo.com>
2014-08-01 00:32:48 -0500

    yay designs

---
+++ vacstatusdesign/js/app.js
@@ -0,0 +1,17 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+
+if (Modernizr.localstorage) {
+  var vacstatusAlertBox = localStorage.getItem('vacstatusAlertBox');
+
+  if(vacstatusAlertBox === 'true') {
+    $('.vacstatus-alert-box').remove();
+  }
+}
+
+$('.vacstatus-alert-box').on('close.fndtn.alert-box', function(event) {
+  if (Modernizr.localstorage) {
+    localStorage.setItem('vacstatusAlertBox', 'true');
+  }
+});
