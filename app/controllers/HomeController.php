ffca55c8b4ba3c8e861a63dd817b6c662d58b14c
Jung Oh <Jung3o@yahoo.com>
2014-08-08 18:54:35 -0500

    ugh so lazy

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -11,8 +11,6 @@ class HomeController extends BaseController {
    */
   public function indexAction()
   {
-
-    dd(App::environment());
     // var_dump(Steam::toBigId(Steam::toSmallId('76561198020317127')));
     // var_dump(Steam::cURLSteamAPI('ban', array('76561198020317127','76561198047414609')));
     return View::make('main/index');
