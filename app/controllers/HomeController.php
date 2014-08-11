16031962a37e409dc9df174494f92224dcfec7c7
Jung Oh <Jung3o@yahoo.com>
2014-08-11 17:51:20 -0500

    google analytics

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,8 +17,6 @@ class HomeController extends BaseController {
       $req = array($uorl, $list);
     }
     $userList = UserList::getListType($req);
-    // var_dump(Steam::toBigId(Steam::toSmallId('76561198020317127')));
-    // var_dump(Steam::cURLSteamAPI('ban', array('76561198020317127','76561198047414609')));
     return View::make('main/index', array('userList' => $userList));
   }
 
