7ea86f4bc76f44033afa7b495d2634548221409f
Jung is Awesome <jung3o@yahoo.com>
2014-10-03 01:09:37 +0000

    accidently removed env

---
+++ .env.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+  'STEAM_API' => '', // Valve's Steam Web API. Register for one at http://steamcommunity.com/dev/apikey
+
+  'DATABASE_TABLE' => 'vacstatus',
+  'DATABASE_USER' => 'root',
+  'DATABASE_PASS' => ''
+
+);
