b1aa04bc86bc694d2bca1fc40d21d79dd7e84958
Jung Oh <jung3o@yahoo.com>
2014-10-09 03:46:21 +0000

    im so bad at this

--- app/controllers/DonationController.php
+++ app/controllers/DonationController.php
@@ -19,16 +19,16 @@ class DonationController extends \BaseController {
 
     $listener->setVerifier($verifier);
 
-    $listener->listen(function() use ($listener) {
+    $listener->listen(function() use ($listener, $ipnMessage) {
         // on verified IPN (everything is good!)
       $resp = $listener->getVerifier()->getVerificationResponse();
-      Log::info('stuff123', (array) $resp);
+      Log::info('stuff123', (array) $ipnMessage);
       dd();
-    }, function() use ($listener) {
+    }, function() use ($listener, $ipnMessage) {
         // on invalid IPN (somethings not right!)
       $report = $listener->getReport();
       $resp = $listener->getVerifier()->getVerificationResponse();
-      Log::info('stuff', (array) $resp);
+      Log::info('stuff boo', (array) $ipnMessage);
       dd();
     });
   }
