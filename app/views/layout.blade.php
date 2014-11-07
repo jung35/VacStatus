95b52050a2cafc4bb4d8df2a58e9b7552421c311
Jung Oh <Jung3o@yahoo.com>
2014-11-07 12:56:00 -0600

    nothing wrong here...

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -276,8 +276,8 @@
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-50795838-1', 'vacstat.us');
-      // ga('require', 'displayfeatures');
+      ga('create', 'UA-50795838-1', 'auto');
+      ga('require', 'displayfeatures');
       ga('send', 'pageview');
       @if(Auth::check())
       ga('set’' '&uid', Auth::User()->getId());
