0b1628449af8076c3a2d56fb6755c5ec88a01a44
sgtaziz <sgtaziz013@gmail.com>
2014-12-28 20:25:38 -0800

    Extra check just to make sure

--- app/views/list/listRow.blade.php
+++ app/views/list/listRow.blade.php
@@ -8,7 +8,7 @@
         {{{ $UserListProfile->display_name }}}
       </span>
     </a>
-    @if(!is_null($UserListProfile->profile_description))
+    @if(!is_null($UserListProfile->profile_description) && $UserListProfile->profile_description != '')
     <a href="#" onClick="$('#profile_description').fadeToggle(250, function() { $(this).text($(this).text() == '(Description)' ? '{{{ $UserListProfile->profile_description }}}' : '(Description)').fadeToggle(250); });">
       <div id="profile_description">(Description)</div>
     </a>
