35fcad1c1cf3147b324ab699bead1ca7a85fc747
Jung Oh <Jung3o@yahoo.com>
2015-03-06 17:21:39 -0600

    assets

--- gulpfile.js
+++ gulpfile.js
@@ -9,42 +9,61 @@ var gulp         = require('gulp')
 
 elixir.extend('react', function (src, options) {
 
-    var config = this,
-        defaultOptions = {
-            debug : ! config.production,
-            srcDir: config.assetsDir + 'jsx',
-            output: config.jsOutput
-        };
+	var config = this,
+		defaultOptions = {
+			debug : ! config.production,
+			srcDir: config.assetsDir + 'jsx',
+			output: config.jsOutput
+		};
 
-    options = _.extend(defaultOptions, options);
-    src     = "./" + utilities.buildGulpSrc(src, options.srcDir, '**/*.jsx');
+	options = _.extend(defaultOptions, options);
+	src     = "./" + utilities.buildGulpSrc(src, options.srcDir, '**/*.jsx');
 
-    options = _.extend(defaultOptions, options);
+	options = _.extend(defaultOptions, options);
 
-    gulp.task('react', function () {
+	gulp.task('react', function () {
 
-        var onError = function(e) {
-            new notification().error(e, 'React Compilation Failed!');
-            this.emit('end');
-        };
+		var onError = function(e) {
+			new notification().error(e, 'React Compilation Failed!');
+			this.emit('end');
+		};
 
-        return gulp.src(src)
-            .pipe(react(options)).on('error', onError)
-            .pipe(gulpIf(! options.debug, uglify()))
-            .pipe(gulp.dest(options.output))
-            .pipe(new notification().message('React Compiled!'));
-    });
+		return gulp.src(src)
+			.pipe(react(options)).on('error', onError)
+			.pipe(gulpIf(! options.debug, uglify()))
+			.pipe(gulp.dest(options.output))
+			.pipe(new notification().message('React Compiled!'));
+	});
 
-    this.registerWatcher('react', options.srcDir + '/**/*.jsx');
+	this.registerWatcher('react', options.srcDir + '/**/*.jsx');
 
-    return this.queueTask('react');
+	return this.queueTask('react');
+});
+
+
+elixir.extend('img', function (src, options) {
+
+	var config = this,
+		defaultOptions = {
+			debug : ! config.production,
+			srcDir: config.assetsDir + 'img',
+			output: config.jsOutput + '/../img'
+		};
+
+	options = _.extend(defaultOptions, options);
+	src     = "./" + utilities.buildGulpSrc(src, options.srcDir, '/**.*');
+	options = _.extend(defaultOptions, options);
+
+	gulp.task('img', function () {
+		gulp.src(src).pipe(gulp.dest(options.output));
+	});
+
+	this.registerWatcher('img', options.srcDir + '/*.*');
+
+	return this.queueTask('img');
 });
 
 
 elixir(function(mix) {
-    mix.less('app.less');
-});
-
-elixir(function(mix) {
-    mix.react();
+	mix.less('app.less').react().img();
 });
