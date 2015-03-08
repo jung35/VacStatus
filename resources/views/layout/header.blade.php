92f3a21c462d6f1d2a190e1e396367efc45c48a8
Jung Oh <Jung3o@yahoo.com>
2015-03-08 11:12:18 -0500

    hello html

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#vacstatus-navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<div class="visible-xs-block">
+				<a class="navbar-brand" href="#">Brand</a>
+			</div>
+		</div>
+
+		<div class="collapse navbar-collapse" id="vacstatus-navbar">
+			<ul class="nav navbar-nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Most Tracked</a></li>
+				<li><a href="#">Latest Added</a></li>
+				<li><a href="#">Search</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">~button~</a></li>
+				<li><a href="#">~donate~</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
