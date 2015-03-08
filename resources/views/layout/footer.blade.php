8df3d32cd14832a78ac639af3a9e57f2c1fbf239
Jung Oh <Jung3o@yahoo.com>
2015-03-08 11:19:53 -0500

    hello html

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,19 @@
+<div class="footer">
+	<ul class="left-footer">
+		<li><a href="#">Donate</a></li>
+		<li><a href="#">Privacy Policy</a></li>
+		<li><a href="#">Contact</a></li>
+		<li><a href="#">Github</a></li>
+	</ul>
+
+	<div class="right-footer">
+		<p>
+			&copy; 2015 VacStatus<br>
+			Powered By <a href="#">Steam</a>
+		</p>
+	</div>
+</div>
+
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
