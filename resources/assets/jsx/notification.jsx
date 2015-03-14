4d149192ca24ac5d40f09f4ae283b80b8356989e
Jung Oh <Jung3o@yahoo.com>
2015-03-14 09:38:24 -0500

    this is a pretty cool notifications system

--- resources/assets/jsx/notification.jsx
+++ resources/assets/jsx/notification.jsx
@@ -1,47 +1,83 @@
-$(function() {
-	var notif = {
-		element: $('.notification'),
-		interval: 0,
+var notif = {
+	element: $('.notification'),
+	interval: 0,
+	timeEnd: 0,
 
-		add: function(type, message)
+	add: function(type, message)
+	{
+		var newAlert = $('<div />').addClass('alert').addClass('alert-'+type).html(message);
+		notif.element.append(newAlert);
+		notif.interval++;
+		return this;
+	},
+
+	run: function()
+	{
+		var alert = notif.element.find('.alert');
+		var initial = notif.interval;
+
+		var currentTime = +new Date();
+
+		if(currentTime <= notif.timeEnd)
 		{
-			var newAlert = $('<div />').addClass('alert').addClass('alert-'+type).html(message);
-			notif.element.append(newAlert);
-			notif.interval++;
-			return this;
-		},
-
-		run: function()
-		{
-			var alert = notif.element.find('.alert');
-			var initial = notif.interval;
-
-			alert.each(function(k, element)
-			{
-				setTimeout(function()
-				{
-					notif.animate($(element))
-				}, (initial - notif.interval) * 800);
-
-				notif.interval--;
-
-			}.bind(initial));
-		},
-
-		animate: function(notificationItem)
-		{
-			notificationItem.animate({marginLeft:20}, 1000, function()
-			{
-				notificationItem.delay(2000).animate({
-					opacity: 0,
-					marginTop: -1 * (notificationItem.height() + 10)
-				}, 500, function() {
-					notificationItem.remove();
-				});
-			});
+			setTimeout(function() {
+				notif.run()
+			}, notif.timeEnd - currentTime)
+			return;
+		} else {
+			notif.timeEnd = currentTime + (initial - 1) * 400;
 		}
-	}
 
+		alert.each(function(k, element)
+		{
+			setTimeout(function()
+			{
+				notif.animate($(element))
+			}, (initial - notif.interval) * 800);
+
+			notif.interval--;
+
+		}.bind(initial));
+	},
+
+	animate: function(notificationItem)
+	{
+		notificationItem.animate({marginLeft:20}, 1000, function()
+		{
+			notificationItem.delay(2000).animate({
+				opacity: 0,
+				marginTop: -1 * (notificationItem.height() + 10)
+			}, 500, function() {
+				notificationItem.remove();
+			});
+		});
+	}
+}
+
+$(function() {
+	notif
+		.run();
+	notif
+		.add('success', '123')
+		.add('danger', 'asdgf')
+		.add('info', 'asdasda')
+		.add('warning', 'aaaaaaaaaaaa')
+		.run();
+	notif
+		.add('success', '123')
+		.add('danger', 'asdgf')
+		.add('info', 'asdasda')
+		.add('warning', 'aaaaaaaaaaaa')
+		.add('warning', 'aaaaaaaaaaaa')
+		.add('warning', 'aaaaaaaaaaaa')
+		.add('warning', 'aaaaaaaaaaaa')
+		.run();
+	notif
+		.add('success', '123')
+		.add('danger', 'asdgf')
+		.add('info', 'asdasda')
+		.add('warning', 'aaaaaaaaaaaa')
+		.run();
 	notif
 		.add('success', '123')
 		.add('danger', 'asdgf')
