e52ee587abdef6c76f6abb786fdc328d0ae1ba9b
Jung Oh <Jung3o@yahoo.com>
2015-03-16 00:10:19 -0500

    oh lawd. what am i getting into

---
+++ app/Commands/UpdateMultiAlias.php
@@ -0,0 +1,29 @@
+<?php namespace VacStatus\Commands;
+
+use VacStatus\Commands\Command;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class UpdateMultiAlias extends Command implements SelfHandling {
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		//
+	}
+
+}
