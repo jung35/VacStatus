3e0f4d77f7c225f9a00f76c8d29e5934b251a19e
Jung Oh <Jung3o@yahoo.com>
2015-04-29 12:48:25 -0500

    updated ipn even listener

--- app/Http/Controllers/APIv1/DonationController.php
+++ app/Http/Controllers/APIv1/DonationController.php
@@ -8,9 +8,9 @@ use VacStatus\Models\DonationLog;
 use VacStatus\Models\DonationPerk;
 use VacStatus\Models\User;
 
-use PayPal\Ipn\Listener;
-use PayPal\Ipn\Message;
-use PayPal\Ipn\Verifier\CurlVerifier;
+use Mdb\PayPal\Ipn\Event\MessageVerifiedEvent;
+use Mdb\PayPal\Ipn\ListenerBuilder\Guzzle\InputStreamListenerBuilder as ListenerBuilder;
+
 
 use Auth;
 
@@ -20,123 +20,120 @@ class DonationController extends Controller
 {
 	public function index()
 	{
-        $latestDonation = DonationLog::whereStatus('Completed')
-            ->leftjoin('users', 'donation_log.small_id', '=', 'users.small_id')
-            ->orderBy('donation_log.id', 'desc')
-            ->take(10)
-            ->get([
-                'donation_log.original_amount',
+		$latestDonation = DonationLog::whereStatus('Completed')
+			->leftjoin('users', 'donation_log.small_id', '=', 'users.small_id')
+			->orderBy('donation_log.id', 'desc')
+			->take(10)
+			->get([
+				'donation_log.original_amount',
 
-                'users.display_name',
-                'users.small_id',
+				'users.display_name',
+				'users.small_id',
 
-                'users.donation',
-                'users.beta',
-                'users.site_admin',
-            ]);
+				'users.donation',
+				'users.beta',
+				'users.site_admin',
+			]);
 
-        $latestDonationParsed = [];
+		$latestDonationParsed = [];
 
-        foreach($latestDonation as $donation)
-        {
-            $latestDonationParsed[] = [
-                'original_amount' => $donation->original_amount,
-                'display_name' => $donation->display_name,
-                'small_id' => $donation->small_id,
-                'steam_64_bit' => Steam::to64bit($donation->small_id),
+		foreach($latestDonation as $donation)
+		{
+			$latestDonationParsed[] = [
+				'original_amount' => $donation->original_amount,
+				'display_name' => $donation->display_name,
+				'small_id' => $donation->small_id,
+				'steam_64_bit' => Steam::to64bit($donation->small_id),
 
-                'donation' => $donation->donation,
-                'beta' => $donation->beta,
-                'site_admin' => $donation->site_admin,
-            ];
-        }
+				'donation' => $donation->donation,
+				'beta' => $donation->beta,
+				'site_admin' => $donation->site_admin,
+			];
+		}
 
-        $latestDonation = $latestDonationParsed;
+		$latestDonation = $latestDonationParsed;
 
-        $mostDonation = User::where('donation', '>', '0')
-            ->orderBy('donation', 'desc')
-            ->take(10)
-            ->get([
-                'users.display_name',
-                'users.small_id',
+		$mostDonation = User::where('donation', '>', '0')
+			->orderBy('donation', 'desc')
+			->take(10)
+			->get([
+				'users.display_name',
+				'users.small_id',
 
-                'users.donation',
-                'users.beta',
-                'users.site_admin',
-            ]);
+				'users.donation',
+				'users.beta',
+				'users.site_admin',
+			]);
 
-        $mostDonationParsed = [];
+		$mostDonationParsed = [];
 
-        foreach($mostDonation as $donation)
-        {
-            $mostDonationParsed[] = [
-                'display_name' => $donation->display_name,
-                'small_id' => $donation->small_id,
-                'steam_64_bit' => Steam::to64bit($donation->small_id),
+		foreach($mostDonation as $donation)
+		{
+			$mostDonationParsed[] = [
+				'display_name' => $donation->display_name,
+				'small_id' => $donation->small_id,
+				'steam_64_bit' => Steam::to64bit($donation->small_id),
 
-                'donation' => $donation->donation,
-                'beta' => $donation->beta,
-                'site_admin' => $donation->site_admin,
-            ];
-        }
+				'donation' => $donation->donation,
+				'beta' => $donation->beta,
+				'site_admin' => $donation->site_admin,
+			];
+		}
 
-        $mostDonation = $mostDonationParsed;
+		$mostDonation = $mostDonationParsed;
 
-        $donationPerk = DonationPerk::orderBy('amount', 'asc')->get();
+		$donationPerk = DonationPerk::orderBy('amount', 'asc')->get();
 
-        $user = null;
+		$user = null;
 
-        if(Auth::check())
-        {
-            $user = Auth::user()->toArray();
-            unset($user['remember_token']);
-        }
+		if(Auth::check())
+		{
+			$user = Auth::user()->toArray();
+			unset($user['remember_token']);
+		}
 
-        return compact(
-            'user',
-            'latestDonation',
-            'mostDonation',
-            'donationPerk'
-        );
+		return compact(
+			'user',
+			'latestDonation',
+			'mostDonation',
+			'donationPerk'
+		);
 	}
 
-    public function IPN()
-    {
-        $listener = new Listener;
-        $verifier = new CurlVerifier;
-        $ipnMessage = Message::createFromGlobals();
-        $verifier->setIpnMessage($ipnMessage);
-        $verifier->setEnvironment('production');
-        $listener->setVerifier($verifier);
-        $listener->listen(function() use ($listener, $ipnMessage)
-        {
-            $resp = $listener->getVerifier()->getVerificationResponse();
-            $amount = $ipnMessage['mc_gross'];
-            $smallId = $ipnMessage['custom'];
-            $user = "";
+	public function IPNAction()
+	{
+		$listener = new ListenerBuilder;
 
-            if($ipnMessage['payment_status'] != 'Completed') return;
+		$listener->useSandbox();
+		$listener = $listener->build();
 
-            $donationLog = new DonationLog;
-            $donationLog->status = $ipnMessage['payment_status'];
-            $donationLog->amount = $amount - $ipnMessage['mc_fee'];
-            $donationLog->original_amount = $amount;
+		$listener->onVerified(function (MessageVerifiedEvent $event) {
+			$ipnMessage = $event->getMessage();
+			if($ipnMessage['payment_status'] != 'Completed') return;
 
-            if(is_numeric($smallId)) {
-                $user = User::whereSmallId($smallId)->first();
-                if(isset($user->id))
-                {
-                    $user->donation += $amount;
-                    $user->save();
-                    $donationLog->small_id = $smallId;
-                }
-            }
+			$original_amount = $ipnMessage['mc_gross'];
+			$amount_sub = $ipnMessage['mc_fee'];
+			$smallId = $ipnMessage['custom'];
 
-            $donationLog->save();
+			$donationLog = new DonationLog;
+			$donationLog->status = 'Completed';
+			$donationLog->amount = $original_amount - $amount_sub;
+			$donationLog->original_amount = $original_amount;
 
-        }, function() use ($listener, $ipnMessage) {
-            $report = $listener->getReport();
-            $resp = $listener->getVerifier()->getVerificationResponse();
-        });
-    }
+			if(is_numeric($smallId))
+			{
+				$user = User::whereSmallId($smallId)->first();
+				if(isset($user->id))
+				{
+					$user->donation += $original_amount;
+					$user->save();
+					$donationLog->small_id = $smallId;
+				}
+			}
+
+			$donationLog->save();
+		});
+
+		$listener->listen();
+	}
 }
